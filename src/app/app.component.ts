1771afde0ba84c67205ef2079196e9d8713f9371
Samiha Hatem <Samiha.Hatem@citystars.com.eg>
2024-11-02 13:27:19 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CanvasJS } from '@canvasjs/angular-charts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+
+
+  constructor(){
+    CanvasJS.addColorSet("greenShades",
+      [
+      "#17a8e9",
+      "#02385d",
+      "#2E8B57",
+      "#3CB371",
+      "#90EE90"                
+      ]);
+  }
+    
+  chartOptions = {
+    animationEnabled: true,
+    colorSet:"greenShades",
+    data: [{
+      type: "doughnut",
+      yValueFormatString: "#,###.##'%'",
+      dataPoints: [
+        { y: 28, name: "Labour" },
+        { y: 10, name: "Legal" },
+      ]
+    }]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CanvasJSAngularChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
